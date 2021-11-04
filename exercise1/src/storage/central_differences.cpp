d562066cc2c27c4f8fdd184fb512acdadf7b5c7f
st167397 <st167397@stud.uni-stuttgart.de>
2021-11-04 14:42:44 +0100

    Add files via upload

---
+++ exercise1/src/storage/central_differences.cpp
@@ -0,0 +1,15 @@
+#include "central_differences.h"
+#include <cmath>
+
+double CentralDifferences::computeDu2Dx (int i, int j){
+  Du2Dx = ((u(i+1,j)+u(i,j))^2 - (u(i,j)+u(i-1,j)))^2 / 4dx
+}
+double CentralDifferences::computeDuvDx (int i, int j){
+  DuvDx = ((u(i,j+1)+u(i,j))(v(i-1,j)+v(i,j))-(u(i,j)+u(i,j-1))(v(i+1,j-1)+v(i,j-1)))/4dx
+}
+double CentralDifferences::computeDuvDy (int i, int j){
+  DuvDy = ((u(i,j+1)+u(i,j))(v(i-1,j)+v(i,j))-(u(i,j)+u(i,j-1))(v(i+1,j-1)+v(i,j-1)))/4dy
+}
+double CentralDifferences::computeDv2Dy (int i, int j){
+  Dv2Dy = ((v(i+1,j)+v(i+1,j-1))^2 - (v(i,j)+v(i,j-1)))^2 / 4dy
+}
