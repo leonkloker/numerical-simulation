57a4dc14766222e5fa8ed73f90ec86dc8506de59
Maksim <st155695@stud.uni-sttutgart.de>
2021-11-08 22:15:24 +0100

    outputWriterText.h complete and outputWriterParaview.h except vtkSmartPointer

--- exercise1/src/output_writer/output_writer_paraview.h
+++ exercise1/src/output_writer/output_writer_paraview.h
@@ -1,5 +1,25 @@
+#pragma once
+#include <output_writer.h>
 //! Write a file out/output_<fileNo>.vti to be visualized in ParaView.
 //! It contains 10x10 nodes with an artifical pressure field.
 //! This method is only for demonstration purpose and does nothing useful.
 //! However, we will provide similar files, e.g. "output_writer_paraview.h", to be used in the submission code.
 void writeParaviewOutput(int fileNo);
+
+
+class OutputWriterParaview : OutputWriter
+{
+public:
+
+    //! discretization shared pointer to the discretization object that will contain all the data to be written to the file 
+    OutputWriterParaview(std::shared_ptr <Discretization> discretization);
+    
+    // Write current velocities to file, filename is output_<count>.txt 
+    void OutputWriterParaview::writeFile (double currentTime);
+
+private:
+    //! write only current values of pressure to file, filename is pressure_<count>.txt
+    vtkSmartPointer<vtkXMLImageDataWriter> OutputWriterParaview::vtkWriter_;
+
+
+};
--- exercise1/src/output_writer/output_writer_text.h
+++ exercise1/src/output_writer/output_writer_text.h
@@ -1,1 +1,21 @@
-//todo
+#pragma once
+
+#include "output_writer.h"
+
+// Write *.txt files that are useful for debugging. All values are written
+// to the file as they are stored in the field variables, no interpolation takes place. 
+class OutputWriterText : OutputWriter
+{
+public:
+
+    //! default constructor
+    OutputWriterText();
+    
+    // Write current velocities to file, filename is output_<count>.txt 
+    void OutputWriterText::writeFile (double currentTime);
+
+    //! write only current values of pressure to file, filename is pressure_<count>.txt
+    void OutputWriterText::writePressureFile ();
+
+
+};
