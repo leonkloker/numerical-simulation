a667bf52ef253cafdfbd7accff02d1048f2391aa
Maksim <st155695@stud.uni-sttutgart.de>
2021-11-08 21:55:52 +0100

    output_writer.h implemented

--- exercise1/src/output_writer/output_writer.h
+++ exercise1/src/output_writer/output_writer.h
@@ -1,1 +1,23 @@
-//todo
+#pragma once
+
+#include "discretization.h"
+#include <memory>
+
+// Inteface class for writing simulation data output. 
+class OutputWriter
+{
+public:
+
+    //! constructor: discretization shared pointer to the discretization object that will 
+    // contain all the data to be written to the file 
+    OutputWriter(std::shared_ptr <Discretization> discretization);
+
+    //! write current velocities to file, filename is output_<count>.vti
+    virtual void OutputWriter::writeFile (double currentTime) = 0;
+
+protected:
+    
+    std::shared_ptr<Discretization> OutputWriter::discretization_;
+    int OutputWriter::fileNo_;
+
+};
